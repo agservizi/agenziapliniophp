252ee019b17b9e57e654ceb2c5a251e30161e4ac
agservizi <ag.servizi16@gmail.com>
2025-10-31 10:10:43 +0100

    Correggi la configurazione di Tailwind per garantire la compatibilità con l'oggetto window

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,8 @@ $mainClass = isset($mainClass)
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script>
-        tailwind.config = {
+        window.tailwind = window.tailwind || {};
+        window.tailwind.config = {
             theme: {
                 extend: {
                     colors: {
